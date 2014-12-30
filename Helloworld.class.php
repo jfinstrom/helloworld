d4073650e9af5056682707cdb47a40469d869d8a
jfinstrom <james.finstrom@schmoozecom.com>
2014-12-29 21:15:48 -0700

    add license stuff... because you know it's a thing

--- Helloworld.class.php
+++ Helloworld.class.php
@@ -1,4 +1,26 @@
 <?php
+/*
+ * Copyright (c) 2014 "James Finstrom"
+ * http://github.com/jfinstrom
+ * 
+ * This module was written for the FreePBX project at http://freepbx.org
+ * 
+ * This file is part of the helloworld FreePBX demo module
+ * 
+ * helloworld is free software: you can redistribute it and/or modify
+ * it under the terms of the GNU Affero General Public License as
+ * published by the Free Software Foundation, either version 3 of the
+ * License, or (at your option) any later version.
+ * 
+ * This program is distributed in the hope that it will be useful,
+ * but WITHOUT ANY WARRANTY; without even the implied warranty of
+ * MERCHANTABILITY or FITNESS FOR A PARTICULAR PURPOSE.  See the
+ * GNU Affero General Public License for more details.
+ * 
+ * You should have received a copy of the GNU Affero General Public License
+ * along with this program. If not, see <http://www.gnu.org/licenses/>.
+ */
+ 
 class Helloworld implements BMO {
 	//BMO Methods... These are required by BMO.
 	public function __construct($freepbx = null) {
