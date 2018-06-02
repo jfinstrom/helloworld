6cea16316bc1a6d3b25670777c3ce9e627e091fa
James Finstrom <jfinstrom@sangoma.com>
2018-06-02 16:43:08 -0700

    Clean up BMO class

--- Helloworld.class.php
+++ Helloworld.class.php
@@ -1,114 +1,132 @@
 <?php
-//All module classes should be namespaced
 namespace FreePBX\modules;
+use BMO;
+use FreePBX_Helpers;
+use PDO;
+class Helloworld extends FreePBX_Helpers implements BMO {
+	public $FreePBX = null;
 
-//This setting is for AJAX calls. We want calls to be authenticated and so don't want cross origin calls
-$setting = array('authenticate' => true, 'allowremote' => false);
-
-//The class name should match the file name with an upper-case first letter
-class Helloworld implements \BMO {
 	public function __construct($freepbx = null) {
 		if ($freepbx == null) {
 			throw new Exception("Not given a FreePBX Object");
 		}
 		$this->FreePBX = $freepbx;
-		//This is only needed for database stuff. If you are not doing database stuff you don't need this
-		$this->db = $freepbx->Database;
+		$this->Database = $freepbx->Database;
 	}
-	//BMO Methods
-	//use this OR install.php but probably not both
+	/**
+	 * Installer run on fwconsole ma install
+	 *
+	 * @return void
+	 */
 	public function install(){}
-	//use this OR uninstall.php but probably not both
+
+	/**
+	 * Uninstaller run on fwconsole ma uninstall
+	 *
+	 * @return void
+	 */
 	public function uninstall(){}
-	//Required: Can be empty, not yet used
+	
+	/**
+	 * Unused, Removed in 15.0
+	 *
+	 * @return void
+	 */
 	public function backup() {}
-	//Required: Can be empty, not yet used
+
+	/**
+	 * Unused, Removed in 15.0
+	 * @return void
+	 */
 	public function restore($backup) {}
-	//Required Processes $_REQUEST stuff
+	
+	/**
+	 * Processes form submission and pre-page actions.
+	 *
+	 * @param string $page Display name
+	 * @return void
+	 */
 	public function doConfigPageInit($page) {
-		$id = $_REQUEST['id']?$_REQUEST['id']:'';
-		$action = $_REQUEST['action']?$_REQUEST['action']:'';
-		$subject = $_REQUEST['subject']?$_REQUEST['subject']:'';
-		$body = $_REQUEST['body']?$_REQUEST['body']:'';
-		//Handle form submissions
-		switch ($action) {
+		/** getReq provided by FreePBX_Helpers see https://wiki.freepbx.org/x/0YGUAQ */
+		$action = $this->getReq('action','');
+		$id = $this->getReq('id','');
+		$subject = $this->getReq('subject','');
+		$body = $this->getReq('body');
 
-			case 'add':
-				$id = $this->addItem($subject,$body);
-				$_REQUEST['id'] = $id;
-			break;
-			case 'edit':
-				$this->updateItem($id,$subject,$body);
-			break;
-			case 'delete':
-				$this->deleteItem($id);
-				unset($_REQUEST['action']);
-				unset($_REQUEST['id']);
-			break;
+		if('add' == $action){
+			return $this->addItem($subject, $body);
+		}
+
+		if('delete' == $action){
+			return $this->deleteItem($id);
+		}
+		
+		if('edit' == $action){
+			$this->updateItem($id, $subject, $body);
 		}
 	}
-	//Optional: Add buttons to your page(s) Buttons should not be added in html. Note this is a 13+ method.
-	//Prior to 13 you add the button to the html.
+
+	/**
+	 * Adds buttons to the bottom of pages per set conditions
+	 *
+	 * @param array $request $_REQUEST
+	 * @return void
+	 */
 	public function getActionBar($request) {
-		$buttons = array();
-		switch($request['display']) {
-			//this is usually your module's rawname
-			case 'helloworld':
-				$buttons = array(
-					'delete' => array(
-						'name' => 'delete',
-						'id' => 'delete',
-						'value' => _('Delete')
-					),
-					'reset' => array(
-						'name' => 'reset',
-						'id' => 'reset',
-						'value' => _('Reset')
-					),
-					'submit' => array(
-						'name' => 'submit',
-						'id' => 'submit',
-						'value' => _('Submit')
-					)
-				);
-				//We hide the delete button if we are not editing an item. "id" should be whatever your unique element is.
-				if (empty($request['id'])) {
-					unset($buttons['delete']);
-				}
-				//If we are not in the form view lets 86 the buttons
-				if (empty($request['view'])){
-					unset($buttons);
-				}
-			break;
-		}
-		return $buttons;
-	}
-	//Optional: Ajax stuff
-
-	//This method declares valid ajax commands...
-	public function ajaxRequest($req, &$setting) {
-		//The ajax request
-		if ($req == "getJSON") {
-			//Tell BMO This command is valid. If you are doing a lot of actions use a switch
-			return true;
-		}else{
-			//Deny everything else
-			return false;
-		}
-	}
-	//This handles the AJAX via ajax.php?module=helloworld&command=getJSON&jdata=grid
-	public function ajaxHandler() {
-		if($_REQUEST['command'] == 'getJSON'){
-			switch ($_REQUEST['jdata']) {
-				case 'grid':
-					return $this->getList();
-				break;
-
-				default:
-					print json_encode(_("Invalid Request"));
-				break;
+		if('helloworld' == $request['display']){
+			if(!isset($_GET['view'])){
+				return [];
 			}
+			$buttons = [
+				'delete' => [
+					'name' => 'delete',
+					'id' => 'delete',
+					'value' => _('Delete'),
+				],
+				'reset' => [
+					'name' => 'reset',
+					'id' => 'reset',
+					'value' => _("Reset"),
+				],
+				'submit' => [
+					'name' => 'submit',
+					'id' => 'submit',
+					'value' => _("Submit"),
+				],
+			];
+			if(!isset($_GET['id']) || empty($_GET['id'])){
+				unset($buttons['delete']);
+			}
+			return $buttons;
 		}
+	}
+
+	/**
+	 * Returns bool permissions for AJAX commands
+	 * https://wiki.freepbx.org/x/XoIzAQ
+	 * @param string $command The ajax command
+	 * @param array $setting ajax settings for this command typically untouched
+	 * @return bool
+	 */
+	public function ajaxRequest($command, &$setting) {
+		//The ajax request
+		if ("getJSON" == $command ) {
+			return true;
+		}
+		return false;
+	}
+
+	/**
+	 * Handle Ajax request
+	 * @url ajax.php?module=helloworld&command=getJSON&jdata=grid
+	 * 
+	 * @return array
+	 */
+	public function ajaxHandler() {
+		if('getJSON' == $_REQUEST['command'] && 'grid' == $_REQUEST['jdata']){
+			return $this->getList();
+		}
+		return json_encode(['status' => false, 'message' => _("Invalid Request")]);
 	}
 
 
@@ -120,27 +138,28 @@ class Helloworld implements \BMO {
 	 */
 	public function getOne($id){
 		$sql = "SELECT id,subject,body FROM helloworld WHERE id = :id";
-		$stmt = $this->db->prepare($sql);
-		$stmt->bindParam(':id',$id, \PDO::PARAM_INT);
+		$stmt = $this->Database->prepare($sql);
+		$stmt->bindParam(':id', $id, PDO::PARAM_INT);
 		$stmt->execute();
-		$row =$stmt->fetchObject();
-		return array(
+		$row = $stmt->fetchObject();
+		return [
 			'id' => $row->id,
 			'subject' => $row->subject,
 			'body' => $row->body
-			);
+		];
 	}
 	/**
 	 * getList gets a list od subjects and their respective id.
 	 * @return array id => subject
 	 */
 	public function getList(){
-		$ret = array();
-		$sql = 'SELECT id,subject,body from helloworld';
-		foreach ($this->db->query($sql) as $row) {
-			$ret[] = array('id' => $row['id'],'subject' => $row['subject']);
-		}
-		return $ret;
+		$ret = [];
+		$sql = 'SELECT id, subject FROM helloworld';
+		$data = $this->Database->query($sql)->fetchAll(PDO::FETCH_KEY_PAIR);
+		array_walk($data,function(&$value,$key){
+			$value = ['id' => $key, 'subject' => $value];
+		});
+		return $data;
 	}
 	//Module setters these are all custom methods.
 
@@ -151,11 +170,11 @@ class Helloworld implements \BMO {
 	 */
 	public function addItem($subject,$body){
 		$sql = 'INSERT INTO helloworld (subject, body) VALUES (:subject, :body)';
-		$stmt = $this->db->prepare($sql);
+		$stmt = $this->Database->prepare($sql);
 		$stmt->bindParam(':subject', $subject, \PDO::PARAM_STR);
 		$stmt->bindParam(':body', $body, \PDO::PARAM_STR);
 		$stmt->execute();
-		return $this->db->lastInsertId();
+		return $this;
 	}
 	/**
 	 * updateItem Updates the given ID
@@ -166,11 +185,12 @@ class Helloworld implements \BMO {
 	 */
 	public function updateItem($id,$subject,$body){
 		$sql = 'UPDATE helloworld SET subject = :subject, body = :body WHERE id = :id';
-		$stmt = $this->db->prepare($sql);
+		$stmt = $this->Database->prepare($sql);
 		$stmt->bindParam(':subject', $subject, \PDO::PARAM_STR);
 		$stmt->bindParam(':body', $body, \PDO::PARAM_STR);
 		$stmt->bindParam(':id', $id, \PDO::PARAM_INT);
-		return $stmt->execute();
+		$stmt->execute();
+		rerturn $this;
 	}
 	/**
 	 * deleteItem Deletes the given ID
@@ -179,20 +199,30 @@ class Helloworld implements \BMO {
 	 */
 	public function deleteItem($id){
 		$sql = 'DELETE FROM helloworld WHERE id = :id';
-		$stmt = $this->db->prepare($sql);
+		$stmt = $this->Database->prepare($sql);
 		$stmt->bindParam(':id', $id, \PDO::PARAM_INT);
-		return $stmt->execute();
+		$stmt->execute();
+		return $this;
 	}
 
-
-	// Dialplan Methods
-
-	// This method required, what is is used for?
+	/**
+	 * Do we want to add to the dialplan?
+	 * https://wiki.freepbx.org/display/FOP/BMO+Hooks#BMOHooks-DialplanHooks
+	 *
+	 * @return bool or int 500 priority
+	 */
 	Public function myDialplanHooks(){
 		return true;
 	}
 
-	// Method 'doDialplanHook' used to generate Asterisk dialplan
+	/**
+	 * Dialplan generation
+	 *
+	 * @param object $ext The dialplan object we add to
+	 * @param string $engine This will always be asterisk
+	 * @param int $priority 500?
+	 * @return void
+	 */
 	public function doDialplanHook(&$ext, $engine, $priority){
 		$modulename = 'helloworld';
 
@@ -212,27 +242,24 @@ class Helloworld implements \BMO {
 		$ext->add($id, $c, '', new \ext_playback('hello-world'));
 		$ext->add($id, $c, '', new \ext_playback('demo-congrats'));
 		$ext->add($id, $c, 'hangup', new \ext_hangup());
-
 	}
 
-	//Module General Methods
-
-	//View called by page.helloworld.php
+	/**
+	 * This returns html to the main page
+	 *
+	 * @return string html
+	 */
 	public function showPage(){
-		switch ($_REQUEST['view']) {
-			case 'form':
-				if(isset($_REQUEST['id']) && !empty($_REQUEST['id'])){
-					$subhead = _('Edit Item');
-					$content = load_view(__DIR__.'/views/form.php', $this->getOne($_REQUEST['id']));
-				}else{
-					$subhead = _('Add Item');
-					$content = load_view(__DIR__.'/views/form.php');
-				}
-			break;
-			default:
-				$subhead = _('Item List');
-				$content = load_view(__DIR__.'/views/grid.php');
-			break;
+		$subhead = _('Item List');
+		$content = load_view(__DIR__ . '/views/grid.php');
+
+		if('form' == $_REQUEST['view']){
+			$subhead = _('Add Item');
+			$content = load_view(__DIR__ . '/views/form.php');	
+			if(isset($_REQUEST['id']) && !empty($_REQUEST['id'])){
+				$subhead = _('Edit Item');
+				$content = load_view(__DIR__.'/views/form.php', $this->getOne($_REQUEST['id']));
+			}
 		}
 		 echo load_view(__DIR__.'/views/default.php', array('subhead' => $subhead, 'content' => $content));
 	}
